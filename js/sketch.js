2a55c0dc380019748e2d1e83af5bdb46050f92b8
Maria <mariacristinaiosub@protonmail.com>
2022-10-16 19:00:05 +0300

    Add canvas

---
+++ js/sketch.js
@@ -0,0 +1,31 @@
+/*const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.rect(0, 0, 150, 150);
+ctx.stroke();
+
+let prevX;
+let prevY;
+let isDrawing = false;
+
+canvas.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+canvas.addEventListener('mousemove', ({x, y}) => {
+    if (!isDrawing) {
+        prevX = null;
+        prevY = null;
+        return;
+    }
+    if (prevX && prevY) {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+    prevX = x;
+    prevY = y;
+});*/
